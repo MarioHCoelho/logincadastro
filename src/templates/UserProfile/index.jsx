cbb5a259fe7bfe953de91b3f6a95fac6c2eaea3c
MarioHCoelho <mariohenrique2905@gmail.com>
2025-01-14 12:29:58 -0300

    Removed empty <img> tag on "UserProfile"

--- src/templates/UserProfile/index.jsx
+++ src/templates/UserProfile/index.jsx
@@ -1,10 +1,9 @@
-import broly from '../../assets/engraçado1.png';
 import './styles.css';
 import '../../styles/globalstyle.css';
 function UserProfile() {
   return (
     <div>
-      <img src={broly} className="broly" />
+      <h1>User Profile</h1>
     </div>
   );
 }
